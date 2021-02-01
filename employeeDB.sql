0855fdda051d964c8a6ce5ec2be5bfbafd1ac289
Lucpizz <Lucpizz@gmail.com>
2021-02-01 16:36:00 -0500

    updated database removing syntax error

---
+++ employeeDB.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+
+CREATE TABLE department (
+
+id INT(15) PRIMARY KEY NOT NULL,
+dept_name VARCHAR(50)
+
+);
+
+CREATE TABLE roles (
+
+id INT(15) PRIMARY KEY NOT NULL,
+title VARCHAR(50) NOT NULL,
+salary DECIMAL NOT NULL,
+dept_id INT(15) NOT NULL
+
+);
+
+CREATE TABLE employee (
+
+id INT(15) PRIMARY KEY NOT NULL,
+first_name VARCHAR(50)NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+role_id INT(15) NOT NULL,
+manager_id INT(15)
+
+);
+
+
+
+INSERT INTO department (id, dept_name) 
+values (100, "HelpDesk");
+
+
+INSERT INTO roles (id, title, salary, dept_id)
+VALUES (1234, "HelpDesk Agent", 40000.00, 100);
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Tom", "Costas", 1234, 2);
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
