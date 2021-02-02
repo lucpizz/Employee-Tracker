8c496f58e379406d9079e5d34d90e1ff25c87ebb
Lucpizz <Lucpizz@gmail.com>
2021-02-02 18:02:31 -0500

    updated remove employee function and added comments for each function

--- employee_App.js
+++ employee_App.js
@@ -1,16 +1,22 @@
+/*
+
+Intializing both mysql and inquirer frameworks to use for this application.
+
+*/
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+/*
+
+Intializing the msql connection to the database.
+
+*/
+
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "admin",
-
-  // Your password
   password: "Today2021!$",
   database: "employeeDB",
 });
@@ -20,21 +26,41 @@ connection.connect(function (error) {
   employee_App();
 });
 
+/*
+
+The employee_App function is the main code base running the Employee Tracker Appicaiton.
+It uses the inquirer prompt and switch/case to navigate through the selections and calls the
+other functions to display the result from the selection.
+
+*/
+
 function employee_App() {
+  console.log("Welcome to the Employee Management System!");
   inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "Please choose a selection: ",
-      choices: [
-        "View All Employees",
-        "View All Departments",
-        "View All Roles",
-        "Add Employee",
-        "Remove Employee",
-        "Exit Application",
-      ],
-    })
+    .prompt(
+      {
+        name: "action",
+        type: "rawlist",
+        message: "Please choose a selection: ",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Employee",
+          "Remove Employee",
+          "Exit Application",
+        ],
+      },
+      {
+        name: "quit",
+        type: "confirm",
+        message: "Would you like to quit the application?",
+        default: false,
+        when: (answer) => {
+          return answer.action === "Exit Application";
+        },
+      }
+    )
     .then(function (answer) {
       switch (answer.action) {
         case "View All Employees":
@@ -61,11 +87,17 @@ function employee_App() {
           break;
 
         case "Exit Application":
-          exitApp();
+          exitApp(answer);
           break;
       }
     });
 }
+
+/*
+
+The viewEmployeeList queries the database to display the employees.
+
+*/
 
 function viewEmployeeList(cb) {
   let query = "SELECT * FROM employee";
@@ -75,6 +107,12 @@ function viewEmployeeList(cb) {
     cb(data);
   });
 }
+
+/*
+
+The viewDepartmentList function queries the database to display the departments.
+
+*/
 
 function viewDepartmentList() {
   let query = "SELECT * FROM department";
@@ -86,6 +124,12 @@ function viewDepartmentList() {
   });
 }
 
+/*
+
+The viewRolesList function queries the database to display the roles.
+
+*/
+
 function viewRolesList() {
   let query = "SELECT * FROM roles";
 
@@ -95,6 +139,13 @@ function viewRolesList() {
     return employee_App();
   });
 }
+
+/*
+
+This addEmployee function adds an employee into the database.  Uses both inquirer prompt, answer function, and connection query
+to insert new employee into database.
+
+*/
 
 function addEmployee() {
   inquirer
@@ -138,38 +189,90 @@ function addEmployee() {
     });
 }
 
-function removeEmployee() {
-  viewEmployeeList((data) => {
-    let user_array = data.map((user, index) => {
-      return { value: user.id, name: `${user.first_name} ${user.last_name}` };
+/*
+
+The removeEmployee function 
+
+*/
+
+const removeEmployee = async () => {
+  let employee_query;
+  let employee_array;
+  let answer;
+  try {
+    employee_query = await databaseSync(
+      connection,
+      "SELECT id, CONCAT(first_name, ' ', last_name) as name FROM employee",
+      []
+    );
+    employee_array = employee_query.map((elem) => elem.name);
+    answer = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose Employee To Remove: ",
+        name: "name",
+        choices: employee_array,
+      },
+    ]);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  const employeeID = employee_query.filter(
+    (element) => element.name === answer.name
+  );
+
+  if (employeeID.length > 1) {
+    const answers = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose id",
+        name: "id",
+        choices: employeeID.map((element) => element.id),
+      },
+    ]);
+    connection.query(
+      "DELETE FROM employee WHERE id = ?",
+      [answers.id],
+      (error, result) => {
+        if (error) throw error;
+        console.log(`Employee ${answers.id} deleted`);
+        employee_App();
+      }
+    );
+  } else {
+    connection.query(
+      "DELETE FROM employee WHERE id = ?",
+      [employeeID[0].id],
+      (error, result) => {
+        if (error) throw error;
+        console.log(`${employeeID[0].name} deleted from employees`);
+        employee_App();
+      }
+    );
+  }
+};
+
+const databaseSync = (connection, sql, args) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, args, (error, result) => {
+      if (error) reject(error);
+      resolve(result);
     });
-    inquirer
-      .prompt([
-        {
-          type: "list ",
-          chocies: user_array,
-          message: "Please select employee to remove.",
-          name: "removeEmployee",
-        },
-      ])
-      .then((data) => {
-        console.log(data);
-      });
   });
-}
+};
 
 /*
 
-function exitApp() {
-  inquirer.prompt({
-    name: "quit",
-    type: "confirm",
-    message: "Would you like to quit the application?",
-    default: false,
-    when: (answer) => {
-      return answer.choices === "quit";
-    },
-  }).then (function ())
-}
+The exitApp function quits the application.
 
 */
+
+function exitApp(actions) {
+  if (actions.quit === false) {
+    employee_App();
+  } else {
+    connection.end();
+    console.log("Good Bye!");
+  }
+}
